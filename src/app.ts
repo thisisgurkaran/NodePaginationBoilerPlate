4ac09b035e6549d8f55c846d0de651c541f8d363
Gurkaran-Singh <Gurkaran.Singh@coxautoinc.com>
2022-02-04 07:17:39 -0500

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
-// import express from "express";
-// const app = express();
-// const port = 3000;
+import express from "express";
+const app = express();
+const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send({ res: "this is sparta" });
+});
 
-// app.listen(port, () => {
-//   return console.log(`Express is listening at http://localhost:${port}`);
-// });
-
-console.log("hello2 from ts ");
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
